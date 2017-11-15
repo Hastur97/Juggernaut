bad17072b2cea878899f345e5e21789d246f33da
Fabrizio Siciliano <sicilianofabrizio@gmail.com>
2017-11-15 12:20:21 +0100

    Add files via upload

--- armi/AssaultRifle.java
+++ armi/AssaultRifle.java
@@ -2,21 +2,31 @@ package armi;
 
 import unit.Unit;
 
-public class AssaultRifle extends Weapons{
+public class AssaultRifle extends Weapon{
+	private double damTroops;
+	private double damHearth;
+	private double damWater;
+	private double damAir;
 	
 	public AssaultRifle(){
 		super();
 		this.maxAmmo = 30;
 	}
-
+	
+	@Override
 	public boolean canTarget(Unit target) {
-		
-		return false;
+		if(target instanceof Fanteria && target instanceof Vehicle)
+			return true;
 	}
-
+	
+	@Override
 	public void damageTarget(Unit atk, Unit def) {
-		// TODO Auto-generated method stub
-		
+		private int damageatkdef = ;
+		private int damagedefatk = ;
+		if(atk.canTarget(def)==true){
+			atk.setLife(damagedefatk);
+			def.setLife(damageatkdef);
+		}
 	}
 	
 	
---
+++ armi/Weapon.java
@@ -0,0 +1,27 @@
+package armi;
+
+import unit.Unit;
+
+public abstract class Weapon{
+	
+	protected int maxAmmo;
+	private int ammo;
+	
+	//@ensures \old(ammo)==ammo && \old(maxAmmo) == maxAmmo
+	public int getAmmo(){
+		return this.ammo;
+	}
+	
+	public void setAmmo(int ammo){
+		if(this.ammo + ammo <= maxAmmo && this.ammo + ammo >=0)
+			this.ammo += ammo;
+		else if(this.ammo + ammo > maxAmmo)
+			this.ammo = this.maxAmmo;
+		else if(this.ammo + ammo < 0)
+			this.ammo = 0;
+	}
+	
+	public abstract boolean canTarget(Unit target);
+	
+	public abstract void damageTarget(Unit atk, Unit def);
+}
