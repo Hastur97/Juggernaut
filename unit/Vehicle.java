f31fc601a1c22883d41eff5c38a21556d48b0f6a
Robyroc <33685936+Robyroc@users.noreply.github.com>
2017-11-15 11:52:25 +0100

    Fanteria & Vehicle

---
+++ unit/Vehicle.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unit;
+
+/**
+ *
+ * @author Roberto
+ */
+public interface Vehicle extends Unit{
+    public int getFuel();
+    
+    public void setFuel(int fuel);
+}
